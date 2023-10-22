2e32d44de67ea5744ace7063557b5e500d728624
Rodrigo Póvoa <rpovoa29@gmail.com>
2023-10-22 14:55:29 +0100

    menu done

--- config.pl
+++ config.pl
@@ -1,24 +1,39 @@
 drawMenuPlayer:-
+    clear,
     write('                           Hello and Welcome to DuAlma Game.\n\n\n'),
     write(' Select your desired gamemode:\n'),
     write('     1. Human vs Human\n'),
     write('     2. Human vs Bot\n'),
     write('     3. Bot vs Bot\n'),
     write(' Please Insert a Number, with a dot in the end, to select the gamemode.\n'),
-    handleInput(3).
+    handleInputMenuPlayer.
+
+handleInputMenuPlayer:-
+    read(MenuInput),
+    (between(1, 3, MenuInput) ->
+        (assert(gamemode(MenuInput))),
+        (MenuInput =:= 1 -> true ; drawMenuDifficulty)
+    ;
+        write('Wrong Number. Please insert again.\n'),
+        handleInputMenuPlayer
+    ).
+
 
 drawMenuDifficulty:-
+    clear,
     write(' Select your desired difficulty:\n'),
     write('     1. Level Easy\n'),
     write('     2. Level Hard\n'),
     write(' Please Insert a Number, with a dot in the end, to play DuAlma.\n'),
-    handleInput(2).
+    handleInputMenuDifficulty.
 
-handleInput(ValidInputs):-
+handleInputMenuDifficulty:-
     read(MenuInput),
-    (between(1, ValidInputs, MenuInput) ->
-        !
+    (between(1, 2, MenuInput) ->
+        (assert(difficulty(MenuInput))),
+        true
     ;
         write('Wrong Number. Please insert again.\n'),
-        handleInput(ValidInputs)
+        handleInputMenuDifficulty
     ).
+
--- main.pl
+++ main.pl
@@ -1,9 +1,13 @@
 
 %These lines are to load the necessary files to the execution of our game
 :- consult(config).
+:- consult(utils).
 :- use_module(library(between)).
+
 %The play/0 starts the Game
 play:-
-    drawMenuPlayer,
+    drawMenuPlayer, %The drawMenuPlayer/0 draws the menu of the game
+    gamemode(Gamemode), write(Gamemode), nl, 
+    true.
+
     
-    drawMenuDifficulty.
